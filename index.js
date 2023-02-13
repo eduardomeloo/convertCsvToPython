89b9b65bf37f4c8ee01a599fdcdceef637f97d4c
Eduardo Peixoto de Melo <eduardo.peixoto.melo@hotmail.com>
2023-02-13 14:51:35 -0300

    Add a função latin1Encoding() para ajustar a acentuação

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const {PythonShell} = require('python-shell');
 const { knexSqlite } = require('./config/db')
 var fs = require('fs'); 
 const xmlFormat = require('xml-formatter');
+const iconv = require("iconv-lite");
 
 (async () => {
     await PythonShell.run('script.py', null, async function (err, results) {
         const [fileCsv] =  await results
         //console.log(fileCsv)
-        const fileCsvJson = csvToJson.parseSubArray('*',';').getJsonFromCsv(fileCsv);
+        //let teste = await fs.readFileSync(fileCsv, {encoding: "binary"})
+
+        //const fileCsvJson = csvToJson.parseSubArray('*',';').getJsonFromCsv(fileCsv);
+        const fileCsvJson = await csvToJson.latin1Encoding().parseSubArray('*',';').getJsonFromCsv(fileCsv);
         const fileCsvAjustado = (JSON.parse(JSON.stringify(fileCsvJson).replaceAll('C.RESULTADO','RESULTADO')))
 
         const script1 = `SELECT   (CASE WHEN C.EMPRESA = 1 THEN 1 ELSE 101 END) AS EMPRESA, 
@@ -16,13 +20,12 @@ const xmlFormat = require('xml-formatter');
                          FROM     ConversaoXml C
                          GROUP BY (CASE WHEN C.EMPRESA = 1 THEN 1 ELSE 101 END), C.DTCONTABIL`
 
-        //console.log(fileCsvAjustado)
         await knexSqlite('ConversaoXml').del().then().catch(error => console.log(error)) //Zera o BD
         await knexSqlite('ConversaoXml').insert(fileCsvAjustado).then().catch(error => console.log(error)) //Insere os Dados do CSV
+
         const dadosAgrupados = await knexSqlite.raw(script1)
-       // console.log(dadosAgrupados)
         const dados = await knexSqlite('ConversaoXml').orderBy([{ column: 'EMPRESA', order: 'asc' }, { column: 'RESULTADO', order: 'asc' }])
-       // console.log(dados)
+
         let xml = ''
         for(let i = 0; i < dadosAgrupados.length; i++) {
             xml += `<LancamentoImportacao>
@@ -86,6 +89,5 @@ const xmlFormat = require('xml-formatter');
             lineSeparator: '\n'
         }))
         process.exit()
-        //console.log(xmlCompleto)
     })
 })()
